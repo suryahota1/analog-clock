16d1437fe2931ae87903dcb3983d4663fa6cac38
SuryaHota <suryakant.hota2@gmail.com>
2023-05-30 23:49:22 +0530

    finished analog clock

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import "./css/style.css";
 
 function initializeTime () {
-    console.log("Hello");
+    const time = new Date();
+    const hour = -3600 * (time.getHours() % 12);
+    const mins = -60 * time.getMinutes();
+    const seconds = -time.getSeconds();
+
+    document.querySelector("body").style.setProperty('--_dh', `${(hour + mins + seconds)}s`);
+    document.querySelector("body").style.setProperty('--_dm', `${mins + seconds}s`);
+    document.querySelector("body").style.setProperty('--_ds', `${(seconds)}s`);
 }
 
 initializeTime();
